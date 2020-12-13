8a061e95901151502ebfde4f412479a2f16bd6c3
Alexandra Maero <alexandramaero12@gmail.com>
2020-12-13 10:55:21 -0300

    fix style in addBoiler

--- src/components/AddBoiler/AddBoiler.js
+++ src/components/AddBoiler/AddBoiler.js
@@ -22,8 +22,8 @@ export class AddBoiler extends Component {
     }
     render() {
         return (
-            <form onSubmit={this.onSubmit} style={{ display: 'flex', paddingTop: '20px' }} >
-                <input 
+            <form className="addForm" onSubmit={this.onSubmit}>
+                <input className="inputStyle"
                     type="text" 
                     name="typeId" 
                     style={{flex: '1', padding: '3px'}} 
@@ -31,7 +31,7 @@ export class AddBoiler extends Component {
                     value={this.state.typeId}
                     onChange={this.onChange}
                 />
-                <input 
+                <input className="inputStyle"
                     type="text" 
                     name="maintaince_rate" 
                     style={{flex: '1', padding: '3px'}}  
@@ -39,7 +39,7 @@ export class AddBoiler extends Component {
                     value={this.state.maintaince_rate}
                     onChange={this.onChange}
                 />
-                <input 
+                <input className="inputStyle"
                     type="text, number" 
                     name="hour_maintaince_cost" 
                     style={{flex: '1', padding: '3px'}} 
@@ -47,7 +47,7 @@ export class AddBoiler extends Component {
                     value={this.state.hour_maintaince_cost}
                     onChange={this.onChange}
                 />
-                <input 
+                <input className="inputStyle"
                     type="text, number" 
                     name="hour_eventual_cost" 
                     style={{flex: '1', padding: '3px'}} 
@@ -55,11 +55,9 @@ export class AddBoiler extends Component {
                     value={this.state.hour_eventual_cost}
                     onChange={this.onChange}
                 />
-                <input 
+                <input className="btnSubmit"
                     type="submit" 
-                    value="Submit" 
-                    className="btn" 
-                    style={{flex: '1'}} 
+                    value="Add new boiler" 
                 />
             </form>
         )
